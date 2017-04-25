c3e65696a0393d8889fd9434a2f2231c0d63663b
it-coral <coral1212@yahoo.com>
2017-04-25 19:59:19 +0300

    Bread crumb service fixed

--- dist/js/all.js
+++ dist/js/all.js
@@ -1399,26 +1399,37 @@ var duScrollDefaultEasing=function(e){"use strict";return.5>e?Math.pow(2*e,2)/2:
             if (!__is_local)
                 forceSSL(event);
 
-            BreadCrumbService.set(toState.name);
-            $rootScope.crumbs = BreadCrumbService.getCrumbs();
+            var stateName = toState.name;
 
             ngProgressLite.start();
 
-            if (toState.name !== 'logout') {
+            if (toState.name === 'logout') {
+                if (!$rootScope.currentUser) {
+                    event.preventDefault();
+                    $state.go('auth');
+                    stateName = 'auth';
+                    ngProgressLite.done();
+                }
+            } else {
                 if ($rootScope.currentUser) {
                     if (toState.name === 'auth') {
                         event.preventDefault();
                         $state.go('dashboard');
+                        stateName = 'dashboard';
                         ngProgressLite.done();
                     }
                 } else {
                     if (toState.name !== 'auth') {
                         event.preventDefault();
                         $state.go('auth');
+                        stateName = 'auth';
                         ngProgressLite.done();
                     }
                 }
             }
+
+            BreadCrumbService.set(stateName);
+            $rootScope.crumbs = BreadCrumbService.getCrumbs();
         });
 
         $rootScope.$on('$stateChangeSuccess', function(event, toState) {
