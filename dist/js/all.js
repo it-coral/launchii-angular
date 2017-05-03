dffd85e774eb173c478cb3ce4a51b578ceb9e5c9
it-coral <coral1212@yahoo.com>
2017-05-03 13:30:04 +0300

    Converted the seconds to hh:mm:ss on metric box.

--- dist/js/all.js
+++ dist/js/all.js
@@ -3857,6 +3857,36 @@ window.isEmpty = function(obj) {
 (function() {
     'use strict';
 
+    angular
+        .module('app')
+        .filter('secondsTohhmmss', secondsTohhmmss);
+
+    function secondsTohhmmss() {
+        return function(input) {
+            if (!input) {
+                return '00:00:00';
+            }
+
+            var totalSeconds = Math.round(input);
+
+            var hours   = Math.floor(totalSeconds / 3600);
+            var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+            var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+
+            // round seconds
+            seconds = Math.round(seconds * 100) / 100
+
+            var result = (hours < 10 ? "0" + hours : hours);
+            result += ":" + (minutes < 10 ? "0" + minutes : minutes);
+            result += ":" + (seconds  < 10 ? "0" + seconds : seconds);
+            return result;
+        }
+    }
+})();
+
+(function() {
+    'use strict';
+
     angular.module('app.brands', [])
         .factory('BrandService', BrandService);
 
